d8da73df1f85a95e620dbf126467bb099153f696
vishwajeet-coder07 <vishwajeetrajput282@gmail.com>
2025-05-13 23:59:18 +0530

    Add files via upload
    Signed-off-by: vishwajeet-coder07 <vishwajeetrajput282@gmail.com>

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("this code for the ulimate js course")
+let a= 67 ;
+console.log(a);
+ a = ("Vishwajeet")
+ console.log(a); // this shows dynamic process
+
+// LET ,VAR and CONST -:
+  a = 45; //privously safe as the let
+ let b = 30;
+ const c = "vishwajeet Singh chauhan";// const does not change throughtout the code
+ {
+    let b = 'this';
+    console.log(b) //box vlaue print first then global value
+ }
+console.log (b)
+
+// Primitive data type-:
