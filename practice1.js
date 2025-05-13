d83438e86d3a8ac25cb49fb8641dcd54a1acbdda
unknown <vishwajeetrajput282@gmail.com>
2025-05-14 04:08:49 +0530

    add all files

---
+++ practice1.js
@@ -0,0 +1,30 @@
+// Question 1
+let a = 'harry';
+ let b = "6";
+ console.log((a+b));
+
+ // Question 2
+console.log(typeof (a+b));
+
+// Question 3
+const item ={
+    harsh : "friend",
+    nikhil: "roommate",
+    harry : "youtuber",
+}
+// item = 2  assigment to constant is not allowed
+
+//Question 4
+item ['friend'] = "prakhar";
+item ['harry'] = " chutiya";
+
+console.log(item)
+
+//Question 5
+const dictionary= {
+    appreciate: "recognise the full worth of.\n" ,
+    howdah: "Start each day with the Word of the Day in your inbox!\n",
+    yakka: " work, especially work hard\n",
+}
+
+console.log (dictionary)
